f7b0b132c1cbc85f00457c3db9e2afb124f09266
fengor <github@fengors-realm.de>
2015-03-18 21:31:00 +0100

    added list of all weightentries and weightentry for specific date

--- healthdash.py
+++ healthdash.py
@@ -1,7 +1,34 @@
 from bottle import route, run
+import sqlite3
+import re
 
 @route('/hello')
 def hello():
         return "Hello World!"
 
-run(host='localhost', port=8000, debug=True)
+def load_all_weights():
+    conn = sqlite3.connect('healthdash.db')
+    c = conn.cursor()
+    c.execute("SELECT date, weight, bodyfat, leanbodymass FROM bodyweight")
+    result = c.fetchall()
+    return result
+
+def load_weight(datestring):
+    conn = sqlite3.connect('healthdash.db')
+    c = conn.cursor()
+    c.execute("SELECT date, weight, bodyfat, leanbodymass FROM bodyweight WHERE date=?", (datestring,))
+    result = c.fetchall()
+    return result
+
+@route('/weight')
+def weight():
+    weightlist = load_all_weights()
+    return str(weightlist)
+
+@route('/weight/:datestring#\d{4]-\d{2}-\d{2}#', method='GET')
+def show_weight_for_date(datestring):
+    weightentry = load_weight(datestring)
+    return str(weightentry)
+
+
+run(host='localhost', port=8000, debug=True, reloader=True)
