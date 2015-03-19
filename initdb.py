1b86b9c55175427f4f6f9bc585798c275bb274eb
fengor <github@fengors-realm.de>
2015-03-19 07:01:44 +0100

    removed integer id column and used date as primary key

--- initdb.py
+++ initdb.py
@@ -1,6 +1,6 @@
 import sqlite3
 
 con = sqlite3.connect('healthdash.db') # Warning: This file is created in the current directory
-con.execute("CREATE TABLE if not exists bodyweight (id INTEGER PRIMARY KEY, date date NOT NULL, weight numeric NOT NULL, bodyfat numeric, leanbodymass numeric)")
+con.execute("CREATE TABLE if not exists bodyweight (date date PRIMARY KEY, weight numeric NOT NULL, bodyfat numeric, leanbodymass numeric)")
 
 con.commit()
