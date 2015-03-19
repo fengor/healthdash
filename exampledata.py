c119b37584b37be349c48bdcdafacbf17cff9fd6
fengor <github@fengors-realm.de>
2015-03-19 07:47:07 +0100

    dynamic example data

--- exampledata.py
+++ exampledata.py
@@ -1,6 +1,18 @@
 import sqlite3
+import random
+from datetime import datetime, timedelta
+
+startdate = datetime.strptime("2015-01-01","%Y-%m-%d")
+entries = []
+
+for x in range(0, 10):
+    entries.append((startdate+timedelta(days=x), 80+(random.randint(-40,40)/10), 23+random.randint(-5,5)))
+
 
 con = sqlite3.connect('healthdash.db') # Warning: This file is created in the current directory
-con.execute("INSERT INTO bodyweight (date, weight, bodyfat, leanbodymass) VALUES ('2015-01-01', 80.5, 22.7, 62.23)")
+
+c = con.cursor()
+c.executemany("INSERT OR REPLACE INTO bodyweight (date, weight, bodyfat) VALUES (?, ?, ?)", entries)
+c.execute("UPDATE bodyweight SET leanbodymass=(weight-(weight*bodyfat/100))")
 
 con.commit()
